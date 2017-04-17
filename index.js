50af3b8b0b3439fbbd977c051451b1c65d65c65c
Optykan <syang0525@gmail.com>
2017-04-17 14:11:50 -0400

    kill it

--- index.js
+++ index.js
@@ -15,26 +15,46 @@ var Bot={
 	},
 	text : {
 		channel: null
-	}
+	},
+	prefix: "%",
+	connection: null,
+}
 
+var Queue={
+	queue: [],
+	enqueue: function(i){
+		this.queue.push(i);
+	},
+	dequeue: function(){
+		return this.queue.shift();
+	},
+	isEmpty: function(){
+		return this.queue.length == 0;
+	}
 }
 
 Bot.client.on('ready', () => {
-  console.log('I am ready!');
-  Bot.text.channel = Bot.client.channels.get(process.env.BOT_CHANNEL);
-  Bot.voice.channel = Bot.client.channels.get(process.env.BOT_VOICE_CHANNEL);
+	console.log('I am ready!');
+	Bot.text.channel = Bot.client.channels.get(process.env.BOT_CHANNEL);
+	Bot.voice.channel = Bot.client.channels.get(process.env.BOT_VOICE_CHANNEL);
 });
 
 Bot.client.on('message', message => {
-  if (message.content.startsWith("%play")) {
-    Bot.voice.channel.join()
-	 .then(connection => {
-	 	console.log(message);
-	   const stream = ytdl(message.content.split(" ")[1], {filter : 'audioonly'});
-	   const dispatcher = connection.playStream(stream, Bot.streamOptions);
-	 })
-	 .catch(console.error);
-  }
+	//play command
+	if (message.content.startsWith(Bot.prefix+"play")) {
+		Bot.voice.channel.join()
+		.then(connection => {
+			Bot.connection = connection;
+			const stream = ytdl(message.content.split(" ")[1], {filter : 'audioonly'});
+			const dispatcher = connection.playStream(stream, Bot.streamOptions);
+		})
+		.catch(console.error);
+	}
+
+	//kill command
+	else if(message.content.startsWith(Bot.prefix+"kill")){
+		Bot.voice.channel.leave();
+	}
 });
 
 Bot.client.login(process.env.BOT_TOKEN);
