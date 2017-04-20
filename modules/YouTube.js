f0137730ce2ae7b4c77f409c7e62c74dfac25890
Samuel Yang <syang0525@gmail.com>
2017-04-20 18:35:40 -0400

    clean up the yt api, playlist

--- modules/YouTube.js
+++ modules/YouTube.js
@@ -9,20 +9,56 @@ class YouTube {
 		this.opts={
 			key:key
 		};
-				
+	}
+	_fetch(url, params, callback){
+		if (typeof callback !== 'function'){
+			console.warn("no callback function defined for "+url);
+			return false;
+		}
+		var urlencoded = "";
 		
+		//urlencode the params
+		for(var i in params){
+			urlencoded += "&"+i+"="+params[i]; //in the format &[key]=[value]
+		}
+
+		urlencoded = "?"+urlencoded.substring(1);
+
+			fetch(url+urlencoded+'&key='+this.opts.key).then(result => {
+			return result.json(); //ensure a search isn't resulting no videos, making items[0] nil
+		}).then(json => {
+			callback(json);
+		}).catch(console.error);
 	}
 	search (term,callback){
-		return fetch('https://www.googleapis.com/youtube/v3/search?part=snippet&q='+term+'&key='+key)
-		//is this how strings work
-		//			return result.json.items[0].videoId();
-		.then(function(result) { //what???
-			return result.json(); //ensure a search isn't resulting no videos, making items[0] nil
-		})
-		.then(function(json) {
-			callback(json);
-		})
-		.catch(console.error);
+		var params = {
+			part: "snippet",
+			q: term
+		};
+
+		_fetch("https://www.googleapis.com/youtube/v3/search", params, json=>{
+			var url =""; //the result url
+			//do stuff
+			callback(url);
+		});
+	}
+	parsePlaylist(playlistId, callback){
+		var res = [];
+		var params = {
+			part: 'snippet',
+			playlistId: playlistId,
+			maxResults: 50
+		};
+		_fetch(" https://www.googleapis.com/youtube/v3/playlistItems", params, json=>{
+			if(!json.errors && json.items[0]){
+				for(let i=0; i<json.items.length; i++){
+					res.push({
+						title: json.items[i].snippet.title,
+						url: json.items[i].snippet.resourceId.videoId
+					});
+				}
+			}
+		});
 	}
 	
 }
